51c52ac94585218674a2de8c45ceb333bc1a8902
Airton Jr <airtonbjunior@gmail.com>
2016-06-02 16:24:13 -0300

    Connection external database
    I used my hostgator database. I delete the configs strings, but I'm
    using a test database.

--- getBarbecueInfo.php
+++ getBarbecueInfo.php
@@ -4,12 +4,40 @@
 	$query   = $_SERVER['QUERY_STRING'];
 	$input   = json_decode(file_get_contents('php://input'),true);
 
+
+	/* Test Database */
+	$host     = "";
+	$database = "";
+	$user     = "";
+	$pass     = "";
+	/* Test Database */
+
+	$dbh = mysqli_connect ($host, $user, $pass) or die ('I cannot connect to the database because: ' . mysql_error());
+	mysqli_select_db($dbh, $database) or die ('No database');
 	
-	
+	//mysqli_set_charset($link,'utf8');
+	$result = mysqli_query($dbh, 'SELECT * FROM users');
+
+	echo mysqli_num_rows($result) . "\n-------------------------\n";
+
+	for ($i=0; $i<mysqli_num_rows($result); $i++) {
+		echo ($i>0 ? ',' : '').json_encode(mysqli_fetch_object($result)) . "\n";
+	}
+
+
+/* Se quiser logar depois */
+/*
 	foreach($_SERVER as $i => $server) {
     	echo $i , ' -> ', $server, "\n";
 	}
 	
+	echo "\n-------------------------\n";
+
+	foreach($_SERVER['argv'] as $i => $arg) {
+    	echo $i , ' -> ', $arg, "\n";
+	}
+*/
+
 
 	/* what method */
 	switch ($method) {
